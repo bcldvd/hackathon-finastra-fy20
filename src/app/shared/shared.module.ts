ec0af4cb42279a88fc1e8a77c1457007e78cf520
davbocle <david.bocle@misys.com>
2019-12-03 14:28:11 +0100

    merchant validation screen

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ValidCheckboxComponent } from './valid-checkbox.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ValidCheckboxComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ValidCheckboxComponent
   ]
 })
 export class SharedModule { }
